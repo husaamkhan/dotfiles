0559c3cbe5ca72483703bd2b196a518b70c38e91
Husaam Khan <husaamkhan04@gmail.com>
2026-04-24 15:53:51 -0400

    Changed inactive cursor and removed ligatures

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,8 +2,12 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.colors = {
-	cursor_bg = 'white'
+	cursor_bg = 'white',
+	cursor_border = 'white' -- Needed for inactive cursor, when clicked out of window
 }
+
+-- Disables ligatures
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
 return config
 
